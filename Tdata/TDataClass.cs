cdc4eff853f46cd81c0b5f1f40025c4174851b7e
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-07 22:05:19 -0700

    init2

---
+++ Tdata/TDataClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tdata
+{
+    class TDataClass<TData>: IEnumerable<TData>
+    {
+        public IEnumerator<TData> GetEnumerator()
+        {
+            return null;
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return null;
+        }
+    }
+}
